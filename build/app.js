559e8b40770f1d693c4c846bfd017ea2b1f43c2f
keimoriyama <keischwiiz@gmail.com>
2019-08-26 22:31:18 +0900

    builded

--- build/app.js
+++ build/app.js
@@ -7,4 +7,4 @@ var app = express();
 //testを呼び出す
 app.get('/', indexRouter);
 app.get('/firebase', getDataFromFirebase);
-app.listen(3000, function () { return console.log('listening'); });
+app.listen(process.env.PORT || 5000, function () { return console.log('listening'); });
